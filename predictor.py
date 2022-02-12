b6acd4fb29f1eae3031e68c78c89078e56243d45
Richard <richardkorwand@aol.de>
2022-02-12 20:32:06 +0100

    minor changes

--- predictor.py
+++ predictor.py
@@ -5,10 +5,14 @@ import torch.nn.functional as F
 
 
 class Predictor(nn.Module):
-    def __init__(self, l1_neurons, l2_neurons):
+
+    l1_neurons = 28*28
+    l2_neurons = 28*28
+
+    def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(28*28, l1_neurons)
-        self.l2 = nn.Linear(l2_neurons, 10)
+        self.l1 = nn.Linear(28*28, self.l1_neurons)
+        self.l2 = nn.Linear(self.l2_neurons, 10)
         self.actfun = F.relu
 
     def forward(self, x):
@@ -33,16 +37,16 @@ class Predictor(nn.Module):
 
     def validation_step(self, batch):
         images, labels = batch 
-        out = self(images)                    # Generate predictions
-        loss = F.cross_entropy(out, labels)   # Calculate loss
-        acc = accuracy(out, labels)           # Calculate accuracy
+        out = self(images)                   
+        loss = F.cross_entropy(out, labels)  
+        acc = accuracy(out, labels)          
         return {'val_loss': loss, 'val_acc': acc}
         
     def validation_epoch_end(self, outputs):
         batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        epoch_loss = torch.stack(batch_losses).mean()   
         batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        epoch_acc = torch.stack(batch_accs).mean()      
         return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
     
     def epoch_end(self, epoch, result):
@@ -57,3 +61,12 @@ def accuracy(outputs, labels):
 def evaluate(model, val_loader):
     outputs = [model.validation_step(batch) for batch in val_loader]
     return model.validation_epoch_end(outputs)
+
+
+def make_prediction(matrix):
+    model = Predictor()
+    model.load_state_dict(torch.load('model_params.pth'))
+    img = torch.from_numpy(matrix)
+    img = img.unsqueeze(0)
+    img = img.unsqueeze(0)
+    model.make_prediction(img)
